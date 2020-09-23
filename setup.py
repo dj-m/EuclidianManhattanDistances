787b894d270e7a8e2dfbb1e2ea12718948cff371
Domingo José Moronta <domingomoronta@gmail.com>
2020-09-23 10:42:29 -0700

    Created cookiecutter folder structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The application of the Euclidian & Manhattan Distance metrics to the same dataset and visualizations of their differences.',
+    author='Domingo Moronta',
+    license='MIT',
+)
